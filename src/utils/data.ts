52cd9cdb8a6bf36a50b478af6b2280d5b8a600f7
BrankoMuruaga <brankomuruaga253@gmail.com>
2025-06-24 17:08:08 -0300

    Translate content to Spanish and update contact information

---
+++ src/utils/data.ts
@@ -0,0 +1,7 @@
+export const GITHUB_URL = "https://github.com/BrankoMuruaga";
+export const GITHUB_REPOSITORIES_URL =
+  "https://github.com/BrankoMuruaga?tab=repositories";
+export const LINKEDIN_URL =
+  "https://www.linkedin.com/in/branko-muruaga-17b332249/";
+export const GMAIL = "muruagabranko253@gmail.com";
+export const FORMSPREE_URL = "https://formspree.io/f/mqabgkyj";
